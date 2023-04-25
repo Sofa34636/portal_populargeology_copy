7868e22b1aff63f66fec01652145d88223712f3a
ooubree <max774433@gmail.com>
2023-04-25 15:17:09 +1000

    added time fields for models

--- portal_api/models.py
+++ portal_api/models.py
@@ -1,4 +1,38 @@
+
 from django.db import models
+
+ALL_TIMES = (
+        ('bigBang', 'Большой Взрыв'),
+        ('solarSystem', 'Солнечная Система'),
+        ('moonFormation', 'Образование Луны'),
+        ('История Земли', (
+            ('blackEarth', 'Черная Земля'),
+            ('blueEarth', 'Голубая Земля'),
+            ('greyEarth', 'Серая Земля'),
+            ('livingEarth', 'Живая Земля'),
+            ('redEarth', 'Красная Земля'),
+            ('boringBillion', 'Скучный Миллиард'),
+            ('whiteEarth', 'Белая Земля'),
+            ('greenEarth', 'Зеленая Земля'),
+            ('ageOfMan', 'Эпоха Человека'),
+            ('present', 'Настоящее'),
+            ('future', 'Будущее'),
+        )),)
+
+
+EARTH_TIMES = (
+            ('blackEarth', 'Черная Земля'),
+            ('blueEarth', 'Голубая Земля'),
+            ('greyEarth', 'Серая Земля'),
+            ('livingEarth', 'Живая Земля'),
+            ('redEarth', 'Красная Земля'),
+            ('boringBillion', 'Скучный Миллиард'),
+            ('whiteEarth', 'Белая Земля'),
+            ('greenEarth', 'Зеленая Земля'),
+            ('ageOfMan', 'Эпоха Человека'),
+            ('present', 'Настоящее'),
+            ('future', 'Будущее'),
+        )
 
 
 class Article(models.Model):
@@ -8,6 +42,7 @@ class Article(models.Model):
     text = models.TextField()
     src_article = models.CharField(max_length=100)
     src_magazine = models.CharField(max_length=500)
+    time = models.CharField(max_length=13, choices=ALL_TIMES)
 
     def __str__(self):
         return self.title
@@ -18,6 +53,7 @@ class Exhibit(models.Model):
     time_ago = models.CharField(max_length=100)
     image = models.ImageField(upload_to='exhibits', null=True, blank=True)
     text = models.TextField()
+    time = models.CharField(max_length=13, choices=ALL_TIMES)
 
     def __str__(self):
         return self.title
@@ -26,6 +62,7 @@ class Exhibit(models.Model):
 class Video(models.Model):
     time_ago = models.CharField(max_length=100)
     video_source = models.CharField(max_length=100)
+    time = models.CharField(max_length=13, choices=ALL_TIMES)
 
     def __str__(self):
         return self.time_ago
@@ -36,6 +73,7 @@ class Earth(models.Model):
     time_ago = models.CharField(max_length=100)
     texture = models.ImageField(upload_to='textures', null=True, blank=True)
     text = models.TextField()
+    time = models.CharField(max_length=13, choices=EARTH_TIMES)
 
     def __str__(self):
         return self.title
@@ -44,6 +82,7 @@ class Earth(models.Model):
 class Location(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='locations', null=True, blank=True)
+    time = models.CharField(max_length=13, choices=ALL_TIMES)
 
     def __str__(self):
         return self.title
@@ -55,6 +94,7 @@ class Relief(models.Model):
     location = models.CharField(max_length=100)
     image = models.ImageField(upload_to='reliefs', null=True, blank=True)
     text = models.TextField()
+    time = models.CharField(max_length=13, choices=ALL_TIMES)
 
     def __str__(self):
         return self.title
