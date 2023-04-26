32f7b844b615bb5a41e073e49c35ae19a4f2a926
Remund <mr.odnovil@mail.ru>
2023-04-26 13:38:11 +1000

    minor changes

--- frontend/src/components/ToolComponents/Article/Article.tsx
+++ frontend/src/components/ToolComponents/Article/Article.tsx
@@ -8,6 +8,8 @@ import Button from "@mui/material/Button";
 import { useFetchAllArticlesGroupByHook } from "../../../hooks/useFetchAllArticlesGroupByHook";
 import {useGetArticleByIdQuery} from "../../../store/services/ArticleService";
 import {IArticle} from "../../../types/models/IArticle";
+import Grid from '@mui/material/Grid';
+
 
 export const Article = () => {
   const { time: timeState, instrument: instrumentState } = useAppSelector((state) => state.timeLineReducer);
@@ -46,24 +48,30 @@ export const Article = () => {
   return (
     <Layout layoutProps={{ time: timeState, instrument: instrumentState, isFooterDisplayed: false}}>
       <div className="article">
-        <div className='article--main'>
-            <div className="article--main__subtitle">
-                <h4>{thisArticle.time_ago}</h4>
-            </div>
-            <div className="article--main__title">
-                <h1>{thisArticle.title}</h1>
-            </div>
-            <div className="article--main__sources">
-                <ArticleSourcesMenu reference={thisArticle.src_article} magazine={thisArticle.src_magazine}/>
-            </div>
-            <div className="article--main__content">
-                {thisArticle.text}
-            </div>
-        </div>
-        <div className="article--articleVerticalList">
-            { isLoading ? <span>Loading...</span> : <ArticleVerticalList fetchedArticles={fetchedArticles[0] ?? []}
-                                                                         numberOfArticles={fetchedArticles[0].length ?? 0} />}
-        </div>
+          <Grid container spacing={10} className="article-grid">
+              <Grid item xs={8} className='article-grid__left'>
+                    <div className='article-grid--main'>
+                        <div className="article-grid--main__subtitle">
+                            <h4>{thisArticle.time_ago}</h4>
+                        </div>
+                        <div className="article-grid--main__title">
+                            <h1>{thisArticle.title}</h1>
+                        </div>
+                        <div className="article-grid--main__sources">
+                            <ArticleSourcesMenu reference={thisArticle.src_article} magazine={thisArticle.src_magazine}/>
+                        </div>
+                        <div className="article-grid--main__content">
+                            {thisArticle.text}
+                        </div>
+                    </div>
+              </Grid>
+              <Grid item xs={4} className='article-grid__left'>
+                    <div className="article-grid--articleVerticalList">
+                        { isLoading ? <span>Loading...</span> : <ArticleVerticalList fetchedArticles={fetchedArticles[0] ?? []}
+                                                                                     numberOfArticles={fetchedArticles[0].length ?? 0} />}
+                    </div>
+              </Grid>
+          </Grid>
         <div className='article--goBackButton'>
           <Button onClick={() => navigate(`/${timeState}/articles`)} variant="outlined">
               НАЗАД
