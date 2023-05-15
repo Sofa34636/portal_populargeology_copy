c88958e2fe7b2f20634abae16a692c3ad0d24e42
Remund <mr.odnovil@mail.ru>
2023-05-15 18:05:48 +1000

    sciencePublications page improved

--- frontend/src/components/ToolComponents/Article/ArticleScientificPublicationsHierarchyMenu/ArticleScientificPublicationsHierarchyMenu.tsx
+++ frontend/src/components/ToolComponents/Article/ArticleScientificPublicationsHierarchyMenu/ArticleScientificPublicationsHierarchyMenu.tsx
@@ -1,140 +1,74 @@
 import {Accordion, AccordionBody, AccordionHeader, AccordionItem} from "react-headless-accordion";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import React from "react";
+import {useAppSelector} from "../../../../hooks/redux";
+import {getKeyByValue} from "../../../../pages/pageRedirect";
+import {timeTypes} from "../../../../types/timeline";
+import {earthSciPub_Hierarchy} from "../../../../types/scientificPublications";
+import Button from "@mui/material/Button";
+
 
 export const ArticleScientificPublicationsHierarchyMenu = () => {
+    const { time: timeState, instrument: instrumentState } = useAppSelector((state) => state.timeLineReducer);
 
-    const hierarchy = {
-        'Докембрий': {
-            'Архей': {}
-        }
+    const hierarchy = earthSciPub_Hierarchy[getKeyByValue(timeTypes, timeState)]
+
+    const Layer = (props: { layerName: string, layerNumber: number, children?: JSX.Element | JSX.Element[] }) => {
+
+        return (
+            <AccordionItem>
+                {({open}) => (
+                    <>
+                        <AccordionHeader className={`LayerButton Layer${props.layerNumber}`}>
+                            <h3>{props.layerName}</h3>
+                            {open ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
+                        </AccordionHeader>
+
+                        <AccordionBody className={`LayerBody Layer${props.layerNumber}`}>
+                            <div>
+                                <Button className={`LayerDownloadButton Layer${props.layerNumber}`}>
+                                    Загрузить публикации
+                                </Button>
+                            </div>
+                            <div>
+                                {props.children}
+                            </div>
+                        </AccordionBody>
+                    </>
+                )}
+            </AccordionItem>
+        )
     }
 
     return (
+        Object.keys(hierarchy).length == 0 ? <span>Пусто</span> :
         <Accordion className='scientific_publicationsMenu'>
             {
-                Object.keys(hierarchy).map((mainLayer, indexI, mainNest) => (
-                    <AccordionItem key={indexI}>
-                        {({open}) => (
-                            <>
-                                <AccordionHeader className='mainLayerButton'>
-                                    <h3>{mainLayer}</h3>
-                                    {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-                                </AccordionHeader>
-
-                                <AccordionBody className='mainLayerBody'>
+                Object.keys(hierarchy).map((firstLayer, indexI) => (
+                    <Layer layerName={firstLayer} layerNumber={1} key={indexI}>
+                        {
+                            Object.keys(hierarchy[firstLayer]).map((secondLayer, indexJ) => (
+                                <Layer layerName={secondLayer} layerNumber={2} key={indexJ}>
                                     {
-                                        Object.keys(hierarchy[mainLayer]).map((secondLayer, indexJ, secondNest) => (
-                                            <AccordionItem key={indexJ}>
-                                                {({open}) => (
-                                                    <>
-                                                        <AccordionHeader className='secondLayerButton'>
-                                                            <h3>{secondLayer}</h3>
-                                                            {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-                                                        </AccordionHeader>
+                                        Object.keys(hierarchy[firstLayer][secondLayer]).map((thirdLayer, indexK) => (
+                                            <Layer layerName={thirdLayer} layerNumber={3} key={indexK}>
+                                                {
+                                                    Object.keys(hierarchy[firstLayer][secondLayer][thirdLayer]).map((fourthLayer, indexM) => (
+                                                        <Layer layerName={fourthLayer} layerNumber={4} key={indexM}>
 
-                                                        <AccordionBody className='secondLayerBody'>
-                                                            {
-                                                                Object.keys(hierarchy[mainLayer][secondLayer]).map((thirdLayer, indexK, thirdNest) => (
-                                                                    <AccordionItem key={indexK}>
-                                                                        {({open}) => (
-                                                                            <>
-                                                                                <AccordionHeader className='thirdLayerButton'>
-                                                                                    <h3>{thirdLayer}</h3>
-                                                                                    {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-                                                                                </AccordionHeader>
-
-                                                                                <AccordionBody className='thirdLayerBody'>
-                                                                                    {
-                                                                                        Object.keys(hierarchy[mainLayer][secondLayer][thirdLayer]).map((fourthLayer, indexM, fourthNest) => (
-                                                                                            <AccordionItem key={indexM}>
-                                                                                                {({open}) => (
-                                                                                                    <>
-                                                                                                        <AccordionHeader className='fourthLayerButton'>
-                                                                                                            <h3>{fourthLayer}</h3>
-                                                                                                            {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-                                                                                                        </AccordionHeader>
-
-                                                                                                        <AccordionBody className='fourthLayerBody'>
-                                                                                                            {
-                                                                                                                Object.keys(hierarchy[mainLayer][secondLayer][thirdLayer][fourthLayer]).map((value, ind) => (
-                                                                                                                    <span>{value}</span>
-                                                                                                                ))
-                                                                                                            }
-                                                                                                        </AccordionBody>
-                                                                                                    </>
-                                                                                                )}
-                                                                                            </AccordionItem>
-                                                                                        ))
-                                                                                    }
-                                                                                </AccordionBody>
-                                                                            </>
-                                                                        )}
-                                                                    </AccordionItem>
-                                                                ))
-                                                            }
-                                                        </AccordionBody>
-                                                    </>
-                                                )}
-                                            </AccordionItem>
-                                        )
-                                    )}
-                                </AccordionBody>
-                            </>
-                        )}
-                    </AccordionItem>
-                )
-            )}
-            {/*<AccordionItem>*/}
-            {/*    {*/}
-            {/*        ({open}) => {*/}
-            {/*            return (*/}
-            {/*                <>*/}
-            {/*                    <AccordionHeader className='scientific_publicationsMenu--mainLayerButton'>*/}
-            {/*                        <h3 className='scientific_publicationsMenu--mainButton--title'>*/}
-            {/*                            {mainLayer}*/}
-            {/*                        </h3>*/}
-            {/*                        {open ? <ExpandLessIcon/> : <ExpandMoreIcon/>}*/}
-            {/*                    </AccordionHeader>*/}
-
-            {/*                    <AccordionBody>*/}
-            {/*                        <div className="scientific_publicationsMenu__mainLayerBody">*/}
-            {/*                            {*/}
-            {/*                                secondLayerList.map((secondLayer, index) => {*/}
-            {/*                                    return (*/}
-            {/*                                        <AccordionItem key={index}>*/}
-            {/*                                            {*/}
-            {/*                                                ({open}) => {*/}
-            {/*                                                    return (*/}
-            {/*                                                        <>*/}
-            {/*                                                            <AccordionHeader*/}
-            {/*                                                                className='scientific_publicationsMenu__mainLayerBody--secondLayerButton'>*/}
-            {/*                                                                <h3 className='scientific_publicationsMenu__mainLayerBody--secondLayerButton--title'>*/}
-            {/*                                                                    {secondLayer}*/}
-            {/*                                                                </h3>*/}
-            {/*                                                                {open ? <ExpandLessIcon/> : <ExpandMoreIcon/>}*/}
-            {/*                                                            </AccordionHeader>*/}
-
-            {/*                                                            <AccordionBody>*/}
-            {/*                                                                <div className='scientific_publicationsMenu__mainLayerBody--secondLayerBody'>*/}
-            {/*                                                                    Lorem ipsum dolor sit amet.*/}
-            {/*                                                                </div>*/}
-            {/*                                                            </AccordionBody>*/}
-            {/*                                                        </>*/}
-            {/*                                                    )*/}
-            {/*                                                }*/}
-            {/*                                            }*/}
-            {/*                                        </AccordionItem>*/}
-            {/*                                    )*/}
-            {/*                                })*/}
-            {/*                            }*/}
-            {/*                        </div>*/}
-            {/*                    </AccordionBody>*/}
-            {/*                </>*/}
-            {/*            )*/}
-            {/*        }*/}
-            {/*    }*/}
-            {/*</AccordionItem>*/}
+                                                        </Layer>
+                                                    ))
+                                                }
+                                            </Layer>
+                                        ))
+                                    }
+                                </Layer>
+                            ))
+                        }
+                    </Layer>
+                ))
+            }
         </Accordion>
     )
 }
