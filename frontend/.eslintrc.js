7e0973af4f0696ed3620aee0b87f569902be840c
ooubree <max774433@gmail.com>
2023-05-21 16:52:50 +1000

    eslint ts no used vars disable

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -15,7 +15,8 @@ module.exports =  {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
     "@typescript-eslint/no-var-requires": 0,
-    "no-unused-vars": 0,
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
     "react/no-unknown-property": 0,
   },
   settings:  {
