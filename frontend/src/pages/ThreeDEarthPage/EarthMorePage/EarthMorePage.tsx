36d650fbc79a09e4f9dc755e667203359e91146b
ooubree <max774433@gmail.com>
2023-05-23 18:38:15 +1000

    delete one very unuseful string

--- frontend/src/pages/ThreeDEarthPage/EarthMorePage/EarthMorePage.tsx
+++ frontend/src/pages/ThreeDEarthPage/EarthMorePage/EarthMorePage.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import { Layout } from '../../../components/Layout/Layout';
-imporat Grid from '@mui/material/Grid';
+
 import './EarthMorePage.scss';
 import { useAppSelector } from '../../../hooks/redux'
-
-import { useGetEarthByIdQuery } from '../../../store/services/EarthService'
-import { historyOfEarth } from '../../../types/timeline'
-import { CardVerticalList } from '../../../components/CardVerticalList/CardVerticalList'
 
 
 export const  EarthMorePage = () => {
   const { time: timeState, instrument: instrumentState } = useAppSelector((state) => state.timeLineReducer);
-
-  const { data } = useGetEarthByIdQuery(historyOfEarth.indexOf(timeState)+1) // remove for deploy
 
 
   return (
