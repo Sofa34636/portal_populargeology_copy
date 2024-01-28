7c0695f0b9fa88c75c261a6eaedeeffaabdc0ad2
evgenius <mr.odnovil@mail.ru>
2024-01-29 00:03:31 +1000

    url change

--- frontend/src/hooks/useFetchArticlesByPeriod.ts
+++ frontend/src/hooks/useFetchArticlesByPeriod.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
-import {getApiUrl} from "../store/services/getApiUrl";
 
 export const useFetchArticlesByPeriod = async (period: string) => {
     try {
-      return await axios.post(`$https://new.populargeology.ru:8000/t2_json_out.php`, {
+      return await axios.post(`https://new.populargeology.ru:8000/t2_json_out.php`, {
         tage: period,
       })
     } catch (error) {
